b4571d353dd7e4dde7c61134638dcaee692ea81a
jviquerat <jonathan.viquerat@mines-paristech.fr>
2023-04-12 16:13:49 +0200

    W

--- beacon/shkadov/job_eval.sh
+++ beacon/shkadov/job_eval.sh
@@ -3,15 +3,14 @@
 #SBATCH --job-name=drl_shkadov
 #SBATCH --output=drl_shkadov.txt
 #SBATCH --partition=MAIN
-#SBATCH --qos=hpc
+#SBATCH --qos=calcul
 #
-#SBATCH --exclusive
 #SBATCH --nodes 1
-#SBATCH --ntasks 8
+#SBATCH --ntasks 64
 #SBATCH --ntasks-per-core 1
 #SBATCH --threads-per-core 1
-#SBATCH --time=7-00:00:00
+#SBATCH --time=24:00:00
 #
 source /home/jviquerat/scratch/dragonfly/venv/bin/activate
 module load openmpi/4.1.1
-srun dgf --train ppo_buffer.json
+mpirun -n 1 dgf --eval -net results/shkadov_ppo/0/ppo -json ppo_buffer.json -steps 400
